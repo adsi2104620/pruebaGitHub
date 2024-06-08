c441b11784a684840675b2cf9ff6936f119cf9f5
OzKr <ozkr.sierrae@gmail.com>
2024-06-07 21:04:10 -0500

    logica para iniciar sesion

--- Clase-05-06/js/logica.js
+++ Clase-05-06/js/logica.js
@@ -1,2 +1,20 @@
 console.log('Logica conectada');
 
+const usuario = 'Admin'
+const clave = '1234'
+
+
+function pruebaBoton(){
+
+    let user = document.getElementById('user').value
+    let pw = document.getElementById('pw').value
+
+    if(usuario==user && clave==pw){
+        window.open('calculadora.html')  
+    }else{
+        console.log('error de credenciales');
+        
+    }
+
+   
+}
