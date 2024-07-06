904c99962ce80550a0004e92338ab61ef7583013
OzKr <ozkr.sierrae@gmail.com>
2024-07-05 22:14:32 -0500

    funciones de arreglos map,some.find

--- Clase-05-06/js/logica.js
+++ Clase-05-06/js/logica.js
@@ -4,24 +4,69 @@ const usuario = 'admin'
 const clave = '1234'
 let contador =0
 
+let usuarios =[
+    {"usuario":"OzKr", "clave":"1234","edad":15},
+    {"usuario":"admin", "clave":"4567","edad":20},
+    {"usuario":"otro", "clave":"1111","edad":18},
+    {"usuario":"otro", "clave":"1111","edad":10}
+]
+
 let frutas = ['Manzana','Pera','Sandia']
 
-
 function pruebaBoton(){
+    let frutasMayus= frutas.map(fruta=>fruta.toLocaleUpperCase())
 
-    let user = document.getElementById('user').value.toLocaleUpperCase()
-    let pw = document.getElementById('pw').value
+    let usuariosEdad= usuarios.map(usuario=>{
+        return{
+            ...usuario,
+            edadRta:usuario.edad >=18 ? "Mayor Edad" :"Menor Edad"
+        }
+    })
 
-    if(usuario.toLocaleUpperCase()==user && clave==pw){
-        //logueado
-        setUser()
-        window.open('localStorage.html')  
-    }else{
-             
-        bloquearCuenta()
-        console.log('error de credenciales'); 
-    }
+    console.log(usuariosEdad);
 }
+
+
+// function pruebaBoton(){
+//     let user = document.getElementById('user').value
+//     let buscarUsuario= usuarios.find(usuario=> usuario.usuario===user)
+
+//     console.log(buscarUsuario);
+// }
+
+
+// function pruebaBoton(){
+//     let user = document.getElementById('user').value.toLocaleUpperCase()
+//     let pw = document.getElementById('pw').value
+
+//     let usuarioLogueado= usuarios.some(usuario=> usuario.usuario.toLocaleUpperCase()==user && usuario.clave==pw)
+
+//     if(usuarioLogueado){
+//         setUser()
+//         window.open('localStorage.html')  
+//     }else{
+//         bloquearCuenta()
+//         console.log('error de credenciales'); 
+//     }
+
+// }
+
+
+// function pruebaBoton(){
+
+//     let user = document.getElementById('user').value.toLocaleUpperCase()
+//     let pw = document.getElementById('pw').value
+
+//     if(usuario.toLocaleUpperCase()==user && clave==pw){
+//         //logueado
+//         setUser()
+//         window.open('localStorage.html')  
+//     }else{
+             
+//         bloquearCuenta()
+//         console.log('error de credenciales'); 
+//     }
+// }
 //bloquear el acceso
 function bloquearCuenta(){
     let contadorStorage =getStorage()
